11b8e718c2e6f8cd4ec2169642a6914cee3465a4
PaoloWD <paolo.lore.93@gmail.com>
2022-11-02 15:35:32 +0100

    complete bonus

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+const containerEl = document.querySelector(".d-flex");
+const f = "Fizz";
+const b = "Buzz";
+const fb = "FizzBuzz";
+
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    containerEl.innerHTML += `<div class="border border-secondary  bg-secondary width-square d-flex align-items-center justify-content-center">${fb}</div>`;
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    containerEl.innerHTML += `<div class="border border-danger  bg-danger width-square d-flex align-items-center justify-content-center">${b}</div>`;
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    containerEl.innerHTML += `<div class="border border-success  bg-success width-square d-flex align-items-center justify-content-center">${f}</div>`;
+  } else {
+    containerEl.innerHTML += `<div class="border border-primary   bg-primary width-square d-flex align-items-center justify-content-center">${i}</div>`;
   }
 }
