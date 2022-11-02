a5542e75137f47f829296d74ae333b4d05a57606
PaoloWD <paolo.lore.93@gmail.com>
2022-11-02 15:37:34 +0100

    add effect for psycopath

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const fb = "FizzBuzz";
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    containerEl.innerHTML += `<div class="border border-secondary  bg-secondary width-square d-flex align-items-center justify-content-center">${fb}</div>`;
+    containerEl.innerHTML += `<div class="border border-secondary  bg-secondary width-square d-flex align-items-center justify-content-center flip">${fb}</div>`;
   } else if (i % 5 === 0) {
-    containerEl.innerHTML += `<div class="border border-danger  bg-danger width-square d-flex align-items-center justify-content-center">${b}</div>`;
+    containerEl.innerHTML += `<div class="border border-danger  bg-danger width-square d-flex align-items-center justify-content-center flip">${b}</div>`;
   } else if (i % 3 === 0) {
-    containerEl.innerHTML += `<div class="border border-success  bg-success width-square d-flex align-items-center justify-content-center">${f}</div>`;
+    containerEl.innerHTML += `<div class="border border-success  bg-success width-square d-flex align-items-center justify-content-center flip">${f}</div>`;
   } else {
-    containerEl.innerHTML += `<div class="border border-primary   bg-primary width-square d-flex align-items-center justify-content-center">${i}</div>`;
+    containerEl.innerHTML += `<div class="border border-primary   bg-primary width-square d-flex align-items-center justify-content-center flip">${i}</div>`;
   }
 }
